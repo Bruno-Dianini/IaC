9275996af1e7acc4f751bb49ee3d8a2a6a72dfce
Bruno-Dianini <109482332+Bruno-Dianini@users.noreply.github.com>
2024-01-30 23:05:30 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,75 +1,79 @@
-
-
-module "locals" {
-  source   = "./modules/locals"
-  rg-name  = var.rg-name
-  location = var.location
+resource "azurerm_virtual_network" "network" {
+  name                = var.net-name
+  address_space       = ["10.0.0.0/16", "192.168.0.0/16"]
+  location            = var.location
+  resource_group_name = var.rg-name
 }
 
-module "net" {
-  source                  = "./modules/network"
-  rg-name                 = var.rg-name
-  network_interface_ids   = module.net.network_interface_ids
-  azurerm_subnet_ids      = module.net.network_subnet_ids
-  loads                   = module.net.loads
-  networksecuritygroup_id = module.net.networksecuritygroup_id
-
-
-  depends_on = [module.locals]
-
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet-name
+  resource_group_name  = var.rg-name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
-module "storage" {
-  source                  = "./modules/storage"
-  azurerm_subnet_ids      = module.net.network_subnet_ids
-  azurerm_storage_account = module.storage.azurerm_storage_account
-
-  depends_on = [module.net]
+resource "azurerm_public_ip" "publicip" {
+  name                    = var.public-ip
+  location                = var.location
+  resource_group_name     = var.rg-name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+  domain_name_label       = "vmwinservertf"
 }
 
-module "datafactory" {
-  source                  = "./modules/datafactory"
-  azurerm_storage_account = module.storage.azurerm_storage_account
+resource "azurerm_network_interface" "nic" {
+  name                = var.interface-name
+  location            = var.location
+  resource_group_name = var.rg-name
+  enable_ip_forwarding = false
 
-  depends_on = [module.storage]
+  ip_configuration {
+    name                          = var.ip-config
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 
-module "virtual_machine" {
-  source                = "./modules/machine"
-  rg-name               = var.rg-name
-  network_interface_ids = module.net.network_interface_ids
+resource "azurerm_lb" "lb" {
+  name                = var.load-name
+  location            = var.location
+  resource_group_name = var.rg-name
 
-  depends_on = [module.datafactory]
-
+  frontend_ip_configuration {
+    name                 = var.ipfront
+    public_ip_address_id = azurerm_public_ip.publicip.id
+  }
 }
 
-module "virtualscale" {
-  source                  = "./modules/machinescaleset"
-  rg-name                 = var.rg-name
-  networksecuritygroup_id = module.net.networksecuritygroup_id
-  azurerm_subnet_ids      = module.net.network_subnet_ids
-  loads                   = module.net.loads
-
-  depends_on = [module.virtual_machine]
-
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = var.backpoll
 }
 
-module "cosmosdb" {
-  source = "./modules/cosmodb"
 
-  depends_on = [module.virtualscale]
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg
+  location            = var.location
+  resource_group_name = var.rg-name
 }
 
-module "key_vault" {
-  source  = "./modules/keyvault"
-  rg-name = var.rg-name
-
-  depends_on = [module.cosmosdb]
+resource "azurerm_network_security_rule" "regrasdeentrada" {
+  for_each                    = var.regras_entrada
+  resource_group_name         = var.rg-name
+  name                        = "portentrada_${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  source_port_range           = "*"
+  protocol                    = "Tcp"
+  destination_port_range      = each.value
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
-module "monitora" {
-  source = "./modules/monitor"
-
-  depends_on = [module.key_vault]
+resource "azurerm_subnet_network_security_group_association" "nsgassociacao" {
+  subnet_id                 = var.azurerm_subnet_ids
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
