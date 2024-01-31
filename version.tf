dd40cfcca4314e2f15538d7aa2e610030ac1c19a
Bruno-Dianini <109482332+Bruno-Dianini@users.noreply.github.com>
2024-01-30 22:53:09 -0300

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = "~> 1.7.1"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.58.0"
+      version = ">=3.85.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "= 2.19.1"
     }
   }
 }
